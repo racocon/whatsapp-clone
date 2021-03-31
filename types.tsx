8837b95f322f8ad209932ed6364efca67b9460a1
racocon <diyanah.nadzree@live.com>
2021-04-01 01:55:09 +0800

    Added chat room header

--- types.tsx
+++ types.tsx
@@ -19,19 +19,19 @@ export type TabTwoParamList = {
 };
 
 export type User = {
-  id: String;
-  name: String;
-  imageUri: String;
+  id: string;
+  name: string;
+  imageUri: string;
 };
 
 export type Message = {
-  id: String;
-  content: String;
-  createdAt: String;
+  id: string;
+  content: string;
+  createdAt: string;
 };
 
 export type ChatRoom = {
-  id: String;
+  id: string;
   users: [User];
   lastMessage: Message;
 };
